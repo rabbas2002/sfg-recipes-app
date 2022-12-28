a27a03334a822d564be6d7614100cdff1a5274fd
rabbas2002 <raza.abbas@valuemedi.com>
2022-12-28 11:55:36 +0530

    Initial Project

---
+++ src/main/java/com/example/springrecipesapp/SpringRecipesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrecipesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springrecipesapp/SpringRecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrecipesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
